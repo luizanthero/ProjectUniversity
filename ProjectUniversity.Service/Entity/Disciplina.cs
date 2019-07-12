8994d053df72cc4a3bab1964bb1fd48a5610320b
Luiz Anthero <luizanthero@icloud.com>
2019-07-12 12:08:49 -0300

    [IMPROVEMENT] Add data annotations on Disciplina and Professor classes

--- ProjectUniversity.Service/Entity/Disciplina.cs
+++ ProjectUniversity.Service/Entity/Disciplina.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectUniversity.Service.Entity
 {
     public class Disciplina
     {
+        [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Nome da Disciplina")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
         public string Nome { get; set; }
+
+        [Display(Name = "Período")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
         public string Periodo { get; set; }
 
         public ICollection<ProfessorDisciplina> ProfessorDisciplinas { get; set; }
--- ProjectUniversity.Service/Entity/Professor.cs
+++ ProjectUniversity.Service/Entity/Professor.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectUniversity.Service.Entity
 {
     public class Professor
     {
+        [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Nome do Professor(a)")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
         public string Nome { get; set; }
+
+        [Display(Name = "Sobrenome do Professor(a)")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
         public string Sobrenome { get; set; }
 
         public ICollection<ProfessorDisciplina> ProfessorDisciplinas { get; set; }
