c3280b0289feb17633b5465276b477666c020677
Luiz Anthero <luizanthero@icloud.com>
2019-07-12 13:38:59 -0300

    [IMPROVEMENT] Add data annotation

--- ProjectUniversity.Service/Entity/ProfessorDisciplina.cs
+++ ProjectUniversity.Service/Entity/ProfessorDisciplina.cs
@@ -1,11 +1,15 @@
-﻿namespace ProjectUniversity.Service.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectUniversity.Service.Entity
 {
     public class ProfessorDisciplina
     {
         public Professor Professor { get; set; }
+        [Display(Name = "Professor")]
         public int ProfessorId { get; set; }
 
         public Disciplina Disciplina { get; set; }
+        [Display(Name = "Disciplina")]
         public int DisciplinaId { get; set; }
     }
 }
