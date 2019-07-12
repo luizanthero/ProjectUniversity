590cf2187f715d36ac91b73a652f24638d9d56ae
Luiz Anthero <luizanthero@icloud.com>
2019-07-12 12:20:43 -0300

    [IMPROVEMENT] Add ProfessorDisciplinaRepository class

---
+++ ProjectUniversity.Service/Repository/ProfessorDisciplinaRepository.cs
@@ -0,0 +1,48 @@
+﻿using ProjectUniversity.Service.Context;
+using ProjectUniversity.Service.Entity;
+using ProjectUniversity.Service.Interface;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ProjectUniversity.Service.Repository
+{
+    public class ProfessorDisciplinaRepository : IRepository<ProfessorDisciplina>
+    {
+        private readonly ServiceContext _context;
+
+        public ProfessorDisciplinaRepository()
+        {
+            _context = new ServiceContext();
+        }
+
+        public void Create(ProfessorDisciplina entity)
+        {
+            _context.ProfessorDisciplinas.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public List<ProfessorDisciplina> GetAll()
+        {
+            return _context.ProfessorDisciplinas.ToList();
+        }
+
+        public ProfessorDisciplina GetById(int? id)
+        {
+            return _context.ProfessorDisciplinas.Find(id);
+        }
+
+        public void Remove(int id)
+        {
+            ProfessorDisciplina professorDisciplina = GetById(id);
+            _context.ProfessorDisciplinas.Remove(professorDisciplina);
+            _context.SaveChanges();
+        }
+
+        public void Update(ProfessorDisciplina entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
